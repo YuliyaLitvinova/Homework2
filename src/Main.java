5c45895e4845c50662b2cd6c14d4acd63ac597d4
yuliya_litvinova <yuliyalitvinovawork@gmail.com>
2023-01-12 01:03:37 +0300

    Task1Done

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1 () {
+        var dog = 8.0 ;
+        var cat = 3.6 ;
+        var paper = 763789 ;
+        System.out.println(" Задача 1");
+        System.out.println(" var dog = " + dog );
+        System.out.println(" var cat = " + cat);
+        System.out.println(" var paper = " + paper);
+    }
+
+    public static void task2 () {
+        System.out.println(" Задача 2");
+
+    }
+    public static void task3 () {
+
+        System.out.println();
+
+    }
+}
